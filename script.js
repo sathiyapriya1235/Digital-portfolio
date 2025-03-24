f2170a91947ee63079553506635b9aab47a09eb9
sathiyapriya1235 <2322ja47@kgcas.com>
2025-03-24 09:55:50 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// Form submission handler
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const message = e.target.message.value;
+
+    if (name && email && message) {
+        alert(`Thank you, ${name}! Your message has been sent.`);
+        e.target.reset();
+    } else {
+        alert("Please fill out all fields.");
+    }
+});
